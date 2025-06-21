bb60fb5afbe2282332e26d62b6b3218773e69a7e
Eugene Chan <eugene@radpair.com>
2025-06-21 02:31:21 +0000

    remove "construct and send a POST request to find a report."

--- src/app/find-report/page.tsx
+++ src/app/find-report/page.tsx
@@ -159,9 +159,6 @@ export default function FindReportPage() {
         <h1 className="text-3xl font-bold font-headline tracking-tight">
           Find Report
         </h1>
-        <p className="text-muted-foreground">
-          Construct and send a POST request to find a report.
-        </p>
       </div>
 
       <Card>
